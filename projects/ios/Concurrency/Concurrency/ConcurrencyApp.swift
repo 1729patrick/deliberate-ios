45bcf3862530344c63e21626a4c9a91fbec57633
Patrick Battisti Forsthofer <1729patrick@gmail.com>
2023-02-20 15:46:52 +0100

    add concurrency app

---
+++ projects/ios/Concurrency/Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Patrick Battisti Forsthofer on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
